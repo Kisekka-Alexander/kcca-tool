3397fdf802d72dc3fdd079d0e7e0b62575530298
Mugula Abbey <mugieabbeym@gmail.com>
2022-01-31 18:13:27 +0300

    Update School documents

---
+++ kstool/kcca_school_tool/page/teacher/teacher.js
@@ -0,0 +1,7 @@
+frappe.pages['teacher'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Teacher',
+		single_column: true
+	});
+}
--- kstool/setup/doctype/school/test_school.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Veritas Interactive and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestSchool(unittest.TestCase):
-	pass
