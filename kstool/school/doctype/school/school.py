e5138887b23678174e76be33d5b542d405286369
Mugula Abbey <mugieabbeym@gmail.com>
2022-02-01 16:49:10 +0300

    Remove school details

--- kstool/school/doctype/school/school.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Veritas Interactive and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class SchoolDetail(Document):
-	pass
--- kstool/school/doctype/school/test_school.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Veritas Interactive and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestSchoolDetail(unittest.TestCase):
-	pass
