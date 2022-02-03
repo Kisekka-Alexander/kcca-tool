bce0e1e97c617142e8993dd5c7ff457fe47361c6
Mugula Abbey <mugieabbeym@gmail.com>
2022-02-03 16:56:50 +0300

    Update school enrollment

--- kstool/school/doctype/enrollment/enrollment.js
+++ kstool/school/doctype/enrollment/enrollment.js
@@ -2,7 +2,38 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Enrollment', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
+	},
+    school: function (frm){
+        let classes = null;
+        frappe.db.get_doc('School', frm.doc.school).then((data) => {
+            console.log(data.category);
+            if (data.category === 'Secondary')
+            {
+                classes = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6'];
+            }
+            else if (data.category === 'Primary')
+            {
+                classes = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7'];
+            }
+
+            frm.set_df_property('school_class', 'options', classes);
+        }).catch((err) => {
+            console.log(err);
+        });
+    },
+
+    school_class: function (frm){
+        let streams = [];
+        frappe.db.get_list('Stream', {"filters": {"class": frm.doc.school_class}}).then((data) => {
+            if (data.length > 0)
+            {
+                data.forEach((stream) => {
+                    streams.push(stream.name);
+                });
+            }
+            frm.set_df_property('stream', 'options', streams);
+        })
+    },
 });
--- kstool/school/doctype/stream/stream.js
+++ kstool/school/doctype/stream/stream.js
@@ -5,4 +5,22 @@ frappe.ui.form.on('Stream', {
 	// refresh: function(frm) {
 
 	// }
+    school: function (frm){
+        let classes = null;
+        frappe.db.get_doc('School', frm.doc.school).then((data) => {
+            console.log(data.category);
+            if (data.category === 'Secondary')
+            {
+                classes = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6'];
+            }
+            else if (data.category === 'Primary')
+            {
+                classes = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7'];
+            }
+
+            frm.set_df_property('school_class', 'options', classes);
+        }).catch((err) => {
+            console.log(err);
+        });
+    },
 });
