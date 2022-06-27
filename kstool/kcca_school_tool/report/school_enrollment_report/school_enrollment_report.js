b538ac83fe0b57d9a3e27537264a5adaa2c1e351
Kisekka-Alexander <kkalexander717@gmail.com>
2022-06-27 18:27:47 +0300

    Add other filters

--- kstool/kcca_school_tool/report/school_enrollment_report/school_enrollment_report.js
+++ kstool/kcca_school_tool/report/school_enrollment_report/school_enrollment_report.js
@@ -5,11 +5,57 @@
 frappe.query_reports["School Enrollment Report"] = {
 	
 	"filters": [
+		{
+            fieldname: 'status',
+            label: __('Status'),
+            fieldtype: 'Select',
+			options: [
+				'All',
+				'Active',
+				'Suspended',
+				'Transfered',
+				'Dropped Out',
+			]
+           
+        },
         {
             fieldname: 'nationality',
             label: __('Nationality'),
             fieldtype: 'Link',
             options: 'Country'
+           
+        },
+		{
+            fieldname: 'is_refugee',
+            label: __('Is Refugee'),
+			fieldtype: 'Select',
+			options: [
+				'All',
+				'Yes',
+				'No'
+			]
+           
+        },
+		{
+            fieldname: 'is_special_needs',
+            label: __('Is Special Needs'),
+			fieldtype: 'Select',
+			options: [
+				'All',
+				'Yes',
+				'No'
+			]
+           
+        },
+		{
+            fieldname: 'is_non_ugandan',
+            label: __('Is Foreigner'),
+            fieldtype: 'Select',
+			options: [
+				'All',
+				'Yes',
+				'No'
+			]
            
         },
 		{
